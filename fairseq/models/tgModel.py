78b5a46f0cdc28bd6285864e5b50eaf8f6585538
Pramit Mallick <pramitmallick@gmail.com>
2018-12-14 00:52:25 -0500

    fix

--- fairseq/models/tgModel.py
+++ fairseq/models/tgModel.py
@@ -6,7 +6,7 @@ from fairseq import options, utils
 from fairseq.modules import AdaptiveSoftmax
 from . import (
     FairseqEncoder, FairseqIncrementalDecoder, FairseqModel, register_model,
-    register_model_architecture
+    register_model_architecture, FairseqDecoder
 )
 from fairseq.models import register_model_architecture
 # from .lstm import *
